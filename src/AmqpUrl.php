840faab77a0246799623049a1de16adb5e617768
someniatko <illia.somov@bahnhof.net>
2023-02-08 15:24:36 +0200

    use PHP 8 promoted properties in AmqpUrl

--- src/AmqpUrl.php
+++ src/AmqpUrl.php
@@ -7,23 +7,11 @@ namespace Someniatko\AmqpUrlParser;
 /** @psalm-immutable */
 final class AmqpUrl
 {
-    public ?string $username;
-    public ?string $password;
-    public ?string $host;
-    public ?int $port;
-    public ?string $vhost;
-
     public function __construct(
-        ?string $username,
-        ?string $password,
-        ?string $host,
-        ?int $port,
-        ?string $vhost
-    ) {
-        $this->username = $username;
-        $this->password = $password;
-        $this->host = $host;
-        $this->port = $port;
-        $this->vhost = $vhost;
-    }
+        public ?string $username,
+        public ?string $password,
+        public ?string $host,
+        public ?int $port,
+        public ?string $vhost
+    ) {}
 }
